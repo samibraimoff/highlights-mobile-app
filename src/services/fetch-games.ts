9acd4a4d0b523620d6b7d5dee2f70faf90dfd385
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2025-04-02 05:13:18 -0500

    hide env

--- src/services/fetch-games.ts
+++ src/services/fetch-games.ts
@@ -1,11 +1,7 @@
 export async function fetchGames() {
   console.log("calling fetch games");
-  // const response = await fetch(
-  //   `${process.env.MAIN_URL}/feed/?token=${process.env.HIGHLIGHTS_PUBLIC_KEY}`,
-  // );
-
   const response = await fetch(
-    "https://www.scorebat.com/video-api/v3/feed/?token=MjA1NzM1XzE3NDM1ODI2MTZfNzI5ZTU3MjM0YTA4YWM5OGJjMTk3MDU2YmI5Y2Q2M2Y5NmMyY2MyNg",
+    `${process.env.EXPO_PUBLIC_MAIN_URL}/feed/?token=${process.env.EXPO_PUBLIC_HIGHLIGHTS_PUBLIC_KEY}`,
   );
 
   if (!response.ok) {
