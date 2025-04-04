9ef55aa78e70ed74b4d190a1dd93e28a8755ae1c
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2025-04-04 03:32:06 -0500

    update layout and add search screen

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -4,30 +4,61 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 const TabsLayout = () => {
   return (
     <Tabs
-      screenOptions={({ route }) => ({
+      screenOptions={() => ({
+        tabBarShowLabel: false,
         tabBarStyle: { backgroundColor: "black" },
-        tabBarIcon: ({ color, size }) => {
-          let iconName = "";
-          if (route.name === "(home)") {
-            iconName = "soccer";
-          } else if (route.name === "explore") {
-            iconName = "soccer-field";
-          } else if (route.name === "notifications") {
-            iconName = "trophy";
-          }
-          return (
-            <MaterialCommunityIcons name={iconName} size={size} color={color} />
-          );
-        },
         tabBarActiveTintColor: "#0A84FF",
         tabBarInactiveTintColor: "#8E8E93",
       })}
     >
-      <Tabs.Screen name={"(home)"} options={{ headerShown: false }} />
-      <Tabs.Screen name={"explore"} options={{ title: "Explore" }} />
+      <Tabs.Screen
+        name={"(home)"}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="soccer"
+              size={size * 1.2}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name={"search"}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="magnify"
+              size={size * 1.2}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name={"explore"}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="soccer-field"
+              size={size * 1.2}
+              color={color}
+            />
+          ),
+        }}
+      />
       <Tabs.Screen
         name={"notifications"}
-        options={{ title: "Notifications" }}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="trophy"
+              size={size * 1.2}
+              color={color}
+            />
+          ),
+        }}
       />
     </Tabs>
   );
---
+++ src/app/(tabs)/search.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View className={"flex justify-center items-center"}>
+      <Text className={"text-red-600 text-2xl"}>Search</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
